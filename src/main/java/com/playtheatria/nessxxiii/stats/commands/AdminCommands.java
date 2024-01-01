7e14528c87076b60f5505d8b37a82433e30e6690
JL-III <jesselopez1162@gmail.com>
2024-01-01 12:34:35 -0600

    add task tracker to debug command

--- src/main/java/com/playtheatria/nessxxiii/stats/commands/AdminCommands.java
+++ src/main/java/com/playtheatria/nessxxiii/stats/commands/AdminCommands.java
@@ -2,9 +2,12 @@ package com.playtheatria.nessxxiii.stats.commands;
 
 import com.playtheatria.nessxxiii.stats.managers.StatManager;
 import com.playtheatria.nessxxiii.stats.utils.Utils;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.scheduler.BukkitTask;
 
 public class AdminCommands implements CommandExecutor {
 
@@ -24,8 +27,15 @@ public class AdminCommands implements CommandExecutor {
             sender.sendMessage("Invalid number of arguments.");
             return true;
         }
-        sender.sendMessage("Number of unique logins: " + statManager.getLogins());
-        sender.sendMessage("Time left in day: " + Utils.calculateDelayUntilEndOfDay() + " seconds");
+        sender.sendMessage(ChatColor.LIGHT_PURPLE + "<----------------- debug info start ----------------->");
+        for (BukkitTask task : Bukkit.getScheduler().getPendingTasks()) {
+            if (task.getOwner().getName().equalsIgnoreCase("stats")) {
+                sender.sendMessage("current tasks: " + task.getOwner().getName() + " - task id: " + task.getTaskId());
+            }
+        }
+        sender.sendMessage("number of unique logins: " + statManager.getLogins());
+        sender.sendMessage("time left in day: " + Utils.calculateDelayUntilEndOfDay() + " seconds");
+        sender.sendMessage(ChatColor.LIGHT_PURPLE + "<------------------ debug info end ------------------>");
         return true;
     }
 }
