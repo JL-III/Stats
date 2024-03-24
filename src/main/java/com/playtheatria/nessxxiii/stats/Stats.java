b54098255b5aa6493ddf5dbd727687ced7f62fc6
JL-III <jesselopez1162@gmail.com>
2024-03-24 08:31:10 -0500

    clean up

--- src/main/java/com/playtheatria/nessxxiii/stats/Stats.java
+++ src/main/java/com/playtheatria/nessxxiii/stats/Stats.java
@@ -3,7 +3,6 @@ package com.playtheatria.nessxxiii.stats;
 import com.playtheatria.nessxxiii.stats.commands.AdminCommands;
 import com.playtheatria.nessxxiii.stats.listeners.DayChange;
 import com.playtheatria.nessxxiii.stats.listeners.PlayerLogIn;
-import com.playtheatria.nessxxiii.stats.managers.ConfigManager;
 import com.playtheatria.nessxxiii.stats.managers.StatsManager;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
@@ -14,12 +13,6 @@ public final class Stats extends JavaPlugin {
 
     @Override
     public void onEnable() {
-        ConfigManager configManager = new ConfigManager(this);
-//        if (!Utils.databaseConfigIsValid(configManager)) {
-//            Bukkit.getPluginManager().disablePlugin(this);
-//            return;
-//        }
-        // todo: initialize timers, listeners, commands, etc.
         StatsManager statsManager = new StatsManager(this);
         Objects.requireNonNull(getCommand("stats")).setExecutor(new AdminCommands(statsManager));
         Bukkit.getPluginManager().registerEvents(new PlayerLogIn(statsManager), this);
